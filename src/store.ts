1756023e57a1b3b1c092db5d04fad9b20c058a5f
zhong666 <aa900031@gmail.com>
2020-05-16 04:04:42 +0800

    feat: 新增 mock data

--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,5 @@
 import Vuex, { Store } from 'vuex';
-import { regist as registTodoModule } from './stores/todo';
+import { regist as registTodoModule, Action as TodoAction, ActionPayload as TodoActionPayload } from './stores/todo';
 import { regist as registPomodoroModule } from '@/stores/pomodoro';
 
 export interface State {
@@ -10,6 +10,16 @@ export const createStore = (): Store<State> => {
   const store = new Vuex.Store<State>({})
   registTodoModule(store)
   registPomodoroModule(store)
+  mockData(store)
 
   return store
+}
+
+const mockData = (store: Store<State>) => {
+  Array.from(new Array(10)).forEach((item, i) => {
+    const payload: TodoActionPayload[TodoAction.Add] = {
+      name: `Todo Item ${i + 1}`,
+    }
+    store.dispatch(`todo/${TodoAction.Add}`, payload)
+  });
 }
