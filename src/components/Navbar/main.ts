d707cee2e87e5f513569a5ba75a4427855f94223
zhong666 <aa900031@gmail.com>
2020-05-16 01:33:26 +0800

    feat: 加上關閉按鈕

--- src/components/Navbar/main.ts
+++ src/components/Navbar/main.ts
@@ -18,6 +18,7 @@ export interface Methods {
   handleNavTodoItemClick(): void
   handleNavAnalyticItemClick(): void
   handleNavRingtoneItemClick(): void
+  handleCloseClick(): void
 }
 
 export interface Computed {
@@ -25,17 +26,37 @@ export interface Computed {
 }
 
 export interface Props {
-
+  visibleNavs: boolean
+  visibleClose: boolean
 }
 
 export const enum Event {
   ClickTodo = 'click-todo',
   ClickAnalytic = 'click-analytic',
   ClickRingtone = 'click-ringtone',
+  ClickClose = 'click-close',
+}
+
+export type Handlers = {
+  [Event.ClickTodo](): void
+  [Event.ClickAnalytic](): void
+  [Event.ClickRingtone](): void
+  [Event.ClickClose](): void
 }
 
 const options: ComponentOption = {
   name: 'Navbar',
+
+  props: {
+    visibleNavs: {
+      type: Boolean,
+      required: true,
+    },
+    visibleClose: {
+      type: Boolean,
+      required: true,
+    },
+  },
 
   methods: {
     handleNavTodoItemClick() {
@@ -47,20 +68,30 @@ const options: ComponentOption = {
     handleNavRingtoneItemClick() {
       this.$emit(Event.ClickRingtone)
     },
+    handleCloseClick() {
+      this.$emit(Event.ClickClose)
+    }
   },
 
   render(h) {
     return h('div', { staticClass: 'navbar' }, [
       h('div', { staticClass: 'navbar__actions' }, [
-        h('div', { staticClass: 'navbar__item', on: { click: this.handleNavTodoItemClick } }, [
-          h('span', { staticClass: 'material-icons' }, 'list')
-        ]),
-        h('div', { staticClass: 'navbar__item', on: { click: this.handleNavAnalyticItemClick } }, [
-          h('span', { staticClass: 'material-icons' }, 'insert_chart')
-        ]),
-        h('div', { staticClass: 'navbar__item', on: { click: this.handleNavRingtoneItemClick } }, [
-          h('span', { staticClass: 'material-icons' }, 'library_music')
-        ])
+        this.visibleClose ? [
+          h('div', { staticClass: 'navbar__item', on: { click: this.handleCloseClick } }, [
+            h('span', { staticClass: 'material-icons' }, 'clear')
+          ]),
+        ] : h(),
+        this.visibleNavs ? [
+          h('div', { staticClass: 'navbar__item', on: { click: this.handleNavTodoItemClick } }, [
+            h('span', { staticClass: 'material-icons' }, 'list')
+          ]),
+          h('div', { staticClass: 'navbar__item', on: { click: this.handleNavAnalyticItemClick } }, [
+            h('span', { staticClass: 'material-icons' }, 'insert_chart')
+          ]),
+          h('div', { staticClass: 'navbar__item', on: { click: this.handleNavRingtoneItemClick } }, [
+            h('span', { staticClass: 'material-icons' }, 'library_music')
+          ])
+        ] : h()
       ]),
       h('div', { staticClass: 'navbar__logo' }, 'POMODORO')
     ]);
